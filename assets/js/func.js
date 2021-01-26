d329ce8be3b7cce33a9ebab2e91fea33ef4af258
hirynn <hirynn125@gmail.com>
2021-01-26 20:32:01 +0800

    update

---
+++ assets/js/func.js
@@ -0,0 +1,15 @@
+$("#engtoggle").on('click', function(e) {
+    alert("something");
+})
+
+$("#jptoggle").on('click', function(e) {
+    alert("something");
+})
+
+$("#lightmode").on('click', function(e) {
+    alert("something");
+})
+
+$("#darkmode").on('click', function(e) {
+    alert("something");
+})
