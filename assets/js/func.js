363dc55317bbf9486d1a6264c5046135dd668eac
hirynn <hirynn125@gmail.com>
2021-01-27 17:00:52 +0800

    test dark-light mode fouc fix

--- assets/js/func.js
+++ assets/js/func.js
@@ -1,7 +1,35 @@
+var themes = {
+    darkmode: {
+        "main_bg": "#121212",
+        "main_color": "white",
+        "header_bg": "#682747",
+        "text_color": "rgba(255, 255, 255, 0.6)",
+        "text_alt_color": "#E579AB",
+        "main_section_border_top": "solid 6px #282828",
+        "features_article_border_top": "solid 3px #282828",
+        "footer_bg": "#222222",
+        "wrapper_bg": "#121212",
+    },
+
+    lightmode: {
+        "main_bg": "white", 
+        "main_color": "black",
+        "header_bg": "#2D848A",
+        "text_color": "#d2f2e9",
+        "text_alt_color": "#d2f2e9",
+        "main_section_border_top": "solid 6px #f4f4f4",
+        "features_article_border_top": "solid 3px #f4f4f4",
+        "footer_bg": "#fafafa",
+        "wrapper_bg": "#fff",
+    }
+}
+
+
 $(window).on('hashchange', function(){
+    // handling scroll position when dark/light mode is changed
     var lastScrollTop = Cookies.get("scroll-pos")
     if (typeof (history.pushState) != "undefined") {
-        //history.pushState(null, $("head title").text(), "./index.html"); // for local testing
+        // history.pushState(null, $("head title").text(), "./index.html"); // for local testing
         history.pushState(null, $("head title").text(), "./");
     }
     if (lastScrollTop) {
@@ -15,7 +43,37 @@ $(window).on('hashchange', function(){
 });
 
 $(window).on('beforeunload', function(){
+    // if page is refreshed it should go to top
     window.scrollTo(0, 0);
+});
+
+$(window).on('DOMContentLoaded', function(){
+    // handles rendering of light/dark mdoe 
+    var setTheme = localStorage.getItem("theme")
+    if (setTheme == undefined)
+        localStorage.setItem("theme", "lightmode");
+
+    var root = document.documentElement.style;
+    window.__onSetTheme = function() {};
+    window.__setTheme = function(themeName) {
+        var theme = themes[themeName];
+        var banner = "";
+        if (themeName == "lightmode")
+            banner = "images/bg_l.gif";
+        else 
+            banner = "images/bg_d.gif";
+
+        $("#bg_banner").attr("src", banner);
+
+        Object.keys(theme).forEach(function(key) {
+            // Set global custom properties on root element
+            root.setProperty("--" + key, theme[key]);
+            window.__onSetTheme(theme)
+        });
+    }
+
+    var setTheme = localStorage.getItem("theme");
+    window.__setTheme(setTheme == undefined ? "lightmode" : setTheme);
 });
 
 $("#engtoggle").on('click', function(e) {
@@ -27,37 +85,21 @@ $("#jptoggle").on('click', function(e) {
 })
 
 $("#lightmode").on('click', function(e) {
-    Cookies.set('scroll-pos', $(window).scrollTop());
+    Cookies.set('scroll-pos', $(window).scrollTop(), {expires: 1});
 
-    if ($("#main").hasClass("dark_mode_main")) {
-        $("#bg_banner").attr("src", "images/bg_l.gif");
-        $("#main section").css("border-top", "solid 6px #f4f4f4");
-        $(".features article").css("border-top", "solid 3px #f4f4f4");
-        $("#footer").css("background", "#fafafa");
-        $("#wrapper").css("background", "#fff");
-
-        toggleDarkMode();
+    if (localStorage.getItem("theme") == "darkmode") {
+        localStorage.setItem("theme", "lightmode"); 
+        
+        window.__setTheme(localStorage.getItem("theme"));
     }
 })
 
 $("#darkmode").on('click', function(e) {
-    Cookies.set('scroll-pos', $(window).scrollTop());
+    Cookies.set('scroll-pos', $(window).scrollTop(), {expires: 1});
 
-    if (!$("#main").hasClass("dark_mode_main")) {
-        $("#bg_banner").attr("src", "images/bg_d.gif");
-        $("#main section").css("border-top", "solid 6px #282828");
-        $(".features article").css("border-top", "solid 3px #282828");
-        $("#footer").css("background", "#222222");
-        $("#wrapper").css("background", "#121212");
-
-        toggleDarkMode();
+    if (localStorage.getItem("theme") == "lightmode") {
+        localStorage.setItem("theme", "darkmode")
+        
+        window.__setTheme(localStorage.getItem("theme"));
     }
 })
-
-function toggleDarkMode() {
-    $("#main").toggleClass("dark_mode_main");
-    $("#header").toggleClass("dark_mode_header");
-    $("#header a").toggleClass("dark_mode_text");
-    $("#tagline").toggleClass("dark_mode_text_alt");
-    $("#logo").toggleClass("dark_mode_text");
-}
